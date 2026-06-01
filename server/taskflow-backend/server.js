93856a28c812b92b4b6bf3693d67deb5feac5a30
KimDSL <dinamoularissa@gmail.com>
2026-06-01 22:02:49 +0100

    Jalon 2: Modélisation NoSQL avec Mongoose

--- server/taskflow-backend/server.js
+++ server/taskflow-backend/server.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connexion MongoDB
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connecté'))
+    .catch((err) => console.error('Erreur de connexion à MongoDB:', err));
+
 // Route de test
 app.get('/api/ping', (req, res) => {
   res.json({ message: "Serveur TaskFlow operationnel" });

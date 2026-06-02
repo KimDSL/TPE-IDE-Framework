636d728bba0f6c7b20519fefa850341e5b98c216
KimDSL <dinamoularissa@gmail.com>
2026-06-02 11:13:42 +0100

    Retouche our test

--- server/taskflow-backend/server.js
+++ server/taskflow-backend/server.js
@@ -5,17 +5,17 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173' // Remplacez par l'URL de votre frontend
+    origin: 'http://localhost:5173'
 }));
 
-// Connexion MongoDB
+
 const mongoose = require('mongoose');
 const taskRoutes = require('./routes/taskRoutes');
 
-// Prépare la connexion (utilise une URI locale si `MONGO_URI` n'est pas défini)
+
 const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/taskflow';
 mongoose.set('strictQuery', false);
 mongoose
@@ -26,7 +26,6 @@ mongoose
   })
   .catch((err) => console.error('Erreur de connexion à MongoDB:', err.message));
 
-// Route de test
 app.get('/api/ping', (req, res) => {
   res.json({ message: "Serveur TaskFlow operationnel" });
 });
